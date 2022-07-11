8c8980d67aa938235137ad1e74fb4767c8d54f89
Robert Belan <robertbelan@protonmail.com>
2022-07-11 22:36:11 +0200

    Add client. Restructure code.

--- CheckMyDirs.Api/Program.cs
+++ CheckMyDirs.Api/Program.cs
@@ -1,23 +1,27 @@
 // TODO: Handle long long report responding.
 
-// TODO: Add global exception handler with pretty errorModel type
+// TODO: Change .pseudogit file location to the same as investigated folder and exclude .pseudogit !!
 
 // TODO: Check if path is too long
 // TODO: Check if path is valid 
 
 
-using CheckMyDirs.IoC;
+using CheckMyDirs.Api.IoC;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure log manager
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+    "/nlog.config"));
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-
 // Add custom services
 builder.Services.ConfigureCors();
+builder.Services.ConfigureLoggerService();
 
 // Add handlers
 builder.Services.AddHandlerServices();
@@ -32,6 +36,8 @@ if (app.Environment.IsProduction())
 {
     app.UseHsts();
 }
+
+app.ConfigureExceptionHandler();
 
 app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
